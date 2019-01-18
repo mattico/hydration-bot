84c562552983929d423e4e0b481b16e22e252d86
Matt Ickstadt <mattico8@gmail.com>
2019-01-18 00:49:41 -0600

    Switch to fork of serenity, add to workspace

--- hydration-bot/src/main.rs
+++ hydration-bot/src/main.rs
@@ -21,10 +21,11 @@ mod commands;
 
 use serenity::framework::StandardFramework;
 use serenity::http;
-use serenity::model::id::{ChannelId, UserId};
+use serenity::model::id::{ChannelId, UserId, GuildId};
 use serenity::model::event::ResumedEvent;
 use serenity::model::gateway::Ready;
 use serenity::model::user::User;
+use serenity::model::voice::VoiceState;
 use serenity::prelude::*;
 use std::collections::HashSet;
 use std::env;
@@ -55,24 +56,19 @@ impl EventHandler for Handler {
         info!("Resumed");
     }
 
-    fn channel_recipient_addition(&self, _ctx: Context, _group_id: ChannelId, user: User) {
-        if !user.bot {
-            info!("User joined: {}", user.name);
-            let mut users = self.talking_users.lock().unwrap();
-            users.insert(user.id);
-        }
-    }
-
-    fn channel_recipient_removal(&self, _ctx: Context, _group_id: ChannelId, user: User) {
-        if !user.bot {
-            info!("User exited: {}", user.name);
-            let mut users = self.talking_users.lock().unwrap();
-            users.remove(&user.id);
+    fn voice_state_update(&self, _ctx: Context, _: Option<GuildId>, voice: VoiceState) {
+        info!("{:?}", voice);
+        if voice.channel_id.is_some() {
+            self.talking_users.lock().unwrap().insert(voice.user_id);
+        } else {
+            self.talking_users.lock().unwrap().remove(&voice.user_id);
         }
     }
 }
 
 fn main() {
+    use serenity::client::validate_token;
+
     // This will load the environment variables located at `./.env`, relative to
     // the CWD. See `./.env.example` for an example on how to structure this.
     kankyo::load().expect("Failed to load .env file");
@@ -81,9 +77,10 @@ fn main() {
     //
     // In this case, a good default is setting the environment variable
     // `RUST_LOG` to debug`.
-    env_logger::init().expect("Failed to initialize env_logger");
+    env_logger::init();
 
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    assert!(validate_token(&token).is_ok(), "Token Invalid");
     if let Ok(id) = env::var("CLIENT_ID") {
         println!("Bot Authentication URL:");
         println!("https://discordapp.com/api/oauth2/authorize?client_id={}&permissions={}&scope=bot\n\n\n", id, PERMISSIONS);
